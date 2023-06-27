bb1ba84c3d6dc64c1351eba70ed709def2d682ab
cotton <bmtosss@gmail.com>
2023-06-27 10:16:17 +0900

    1339 추가

--- new!/1339.py
+++ new!/1339.py
@@ -4,17 +4,17 @@ alpha = [input() for _ in range(N)]
 alpha_dict = {} 
 
 for i in range(N): 
-    for j in range(len(alpha[i])): 
-        if alpha[i][j] in alpha_dict: 
-            alpha_dict[alpha[i][j]] += 10 ** (len(alpha[i])-j-1) 
+    for j,a in enumerate(alpha[i]): 
+        if a in alpha_dict: 
+            alpha_dict[a] += 10 ** (len(alpha[i])-j-1) 
         else:   
-            alpha_dict[alpha[i][j]] = 10 ** (len(alpha[i])-j-1)
+            alpha_dict[a] = 10 ** (len(alpha[i])-j-1)
 
 numList = sorted(alpha_dict.values(),reverse=True)
 total = 0
-num = 9
-for i in numList: 
-    total += num * i
-    num -= 1
+n = 9
+for num in numList: 
+    total += n * num
+    n -= 1
 
 print(total)

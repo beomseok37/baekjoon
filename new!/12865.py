c01c300359f83c880f35d8eb9b34c740a3dcc0e8
beomseok <bmtosss@gmail.com>
2024-03-05 10:23:49 +0900

    3월 5일 문제풀이

--- new!/12865.py
+++ new!/12865.py
@@ -1,19 +1,18 @@
-N,K=list(map(int,input().split()))
-W,V=[0],[0]
-dp=[[0 for _ in range(N+1)] for _ in range(K+1)]
-result=0
-for i in range(N):
-    w,v=list(map(int,input().split()))
+N, K = list(map(int, input().split()))
+W, V = [0], [0]
+for _ in range(N):
+    w, v = list(map(int, input().split()))
     W.append(w)
     V.append(v)
 
-for i in range(N):
-    dp[0][i]=0
-    
-for i in range(K+1):
-    for j in range(1,N+1):
-        if W[j]>i:
-            dp[i][j]=dp[i][j-1]
-        else:
-            dp[i][j]=max(dp[i][j-1],dp[i-W[j]][j-1]+V[j])
-print(dp[K][N])
+dp = [[0 for _ in range(K+1)] for __ in range(N+1)]
+result = 0
+for i in range(1, N+1):
+    for j in range(K+1):
+        dp[i][j] = dp[i-1][j]
+        if W[i] > j:
+            continue
+
+        dp[i][j] = max(dp[i][j], dp[i-1][j-W[i]]+V[i])
+
+print(dp[N][K])
